b217a821b42468ea364d406a69202a685864b95f
Igor Guastalla de Lima <igor.lima@sabium.com.br>
2019-02-19 13:40:00 -0300

    test: add two more tests, but one is not passing yet (commented out)

--- tests/unit/vue-simple-timeline.spec.js
+++ tests/unit/vue-simple-timeline.spec.js
@@ -250,4 +250,40 @@ describe('When I create the VueSimpleTimeline component', () => {
             Title example 1
           </span>`)
   })
+
+  it('should have a two way data bind when itemSelected is passed and a card is clicked', () => {
+    const itemSelected = {
+      title: '',
+      content: ''
+    }
+    const wrapper = createTimelineWrapper({ items, itemSelected })
+    const time = wrapper.find('.vue-simple-timeline>section.timeline>ol>li:first-child>.time')
+    time.trigger('click')
+    const expected = {
+      title: 'Title example 1',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio. Nullam non condimentum orci, a cursus magna. Suspendisse tempor rutrum eros, non pellentesque odio commodo eu. Donec at volutpat enim. Vivamus mattis volutpat urna, sit amet vulputate mauris sollicitudin et. Proin consequat at dolor in sodales. Vestibulum vel porta turpis. Pellentesque sollicitudin justo est, ut dapibus felis luctus mollis. Suspendisse feugiat, metus ut auctor dictum, nulla dui fringilla nisl, a pulvinar ipsum justo non lacus. Integer vestibulum sapien metus, et congue felis efficitur iaculis. Aliquam et mi quis nulla molestie elementum. Vestibulum in nibh nibh.'
+    }
+    expect(time.exists()).toBe(true)
+    expect(wrapper.emitted('click')).toBeTruthy()
+    expect(wrapper.emitted('click')).toMatchObject([[expected]])
+  })
+
+  // it('should have a blue border when itemUniqueKey and itemSelected is passed and a card is clicked', () => {
+  //   const itemSelected = {
+  //     title: '',
+  //     content: ''
+  //   }
+  //   const wrapper = createTimelineWrapper({ items, itemSelected, itemUniqueKey: 'title' })
+  //   const time = wrapper.find('.vue-simple-timeline>section.timeline>ol>li:first-child>.time')
+  //   time.trigger('click')
+  //   const expected = {
+  //     title: 'Title example 1',
+  //     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ex dolor, malesuada luctus scelerisque ac, auctor vitae risus. Vivamus risus dolor, faucibus a bibendum quis, facilisis eget odio. Nullam non condimentum orci, a cursus magna. Suspendisse tempor rutrum eros, non pellentesque odio commodo eu. Donec at volutpat enim. Vivamus mattis volutpat urna, sit amet vulputate mauris sollicitudin et. Proin consequat at dolor in sodales. Vestibulum vel porta turpis. Pellentesque sollicitudin justo est, ut dapibus felis luctus mollis. Suspendisse feugiat, metus ut auctor dictum, nulla dui fringilla nisl, a pulvinar ipsum justo non lacus. Integer vestibulum sapien metus, et congue felis efficitur iaculis. Aliquam et mi quis nulla molestie elementum. Vestibulum in nibh nibh.'
+  //   }
+  //   expect(time.exists()).toBe(true)
+  //   expect(wrapper.emitted('click')).toBeTruthy()
+  //   expect(wrapper.emitted('click')).toEqual([[expected]])
+  //   expect(time.classes().length).toBe(2)
+  //   expect(time.classes()).toContainEqual('border-blue')
+  // })
 })
