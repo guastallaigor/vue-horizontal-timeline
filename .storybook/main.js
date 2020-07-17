a243022278f3c3a0d636433653b95d875a330085
guastallaigor <iguastalla@uolinc.com>
2020-07-17 10:52:56 -0300

    chore: update storybook, add css minified build step

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+    stories: ['../stories/**/*.stories.js'],
+    addons: [
+      '@storybook/addon-knobs',
+      '@storybook/addon-docs',
+      '@storybook/addon-actions',
+      '@storybook/addon-viewport/register',
+      '@storybook/addon-a11y'
+    ]
+  };
--- config/storybook/addons.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import '@storybook/addon-actions/register'
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-links/register'
-import '@storybook/addon-notes/register'
-import '@storybook/addon-backgrounds/register'
